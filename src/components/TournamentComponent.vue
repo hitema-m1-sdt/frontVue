f8668bd18c9771e8e591930019149200d1792a32
NicoSar <nicolas.sarrazin88@gmail.com>
2019-07-04 23:45:41 +0200

    Update TournamentComponent.vue

--- src/components/TournamentComponent.vue
+++ src/components/TournamentComponent.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="col-9 mx-auto">
-        <h1>Compétitions</h1><b-button id="show-btn" variant="success" @click="$bvModal.show('bv-modal-example')">Ajouter un compétition</b-button><br /><br />
+        <h1>Compétitions</h1><b-button id="show-btn" variant="success" @click="$bvModal.show('bv-modal-createtournament')">Ajouter un compétition</b-button><br /><br />
         <table class="table table-bordered">
             <tr>
                 <th>Catégorie</th>
                 <th>Nom</th>
                 <th>Adresse</th>
                 <th>Date et horaire</th>
+                <th>Action</th>
             </tr>
             <tr v-for="tournament in tournaments">
-                <td> {{tournament.idCategory}}</td>
+                <td> {{tournament.categories}}</td>
                 <td> {{tournament.name}}</td>
                 <td> {{tournament.localisation}}</td>
-                <td> {{tournament.DateHour}}</td>
+                <td> {{tournament.date}} {{tournament.hour}}</td>
+                <td> <b-button variant="success" @click="$bvModal.show('bv-modal-updatetournament'), editTournament(tournament.id)"><font-awesome-icon icon="edit" /></b-button> | <b-button variant="danger" @click="deleteTournament(tournament.id)"><font-awesome-icon icon="trash" /></b-button></td>
             </tr>
         </table>
 
-        <b-modal id="bv-modal-example" hide-footer v-if="form.categories != []">
+        <b-modal id="bv-modal-createtournament" hide-footer v-if="form.categories != []">
     <template slot="modal-title">
       Ajouter une compétition
     </template>
@@ -67,13 +69,71 @@
           ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button type="submit" variant="primary" @click="$bvModal.hide('bv-modal-createtournament')">Envoyer</b-button>
+      <b-button type="reset" variant="danger">Réinitialiser</b-button>
     </b-form>
       <!----------------------FIN FORM MODAL-------------->
     </div>
-    <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')">Fermer</b-button>
+    <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-createtournament')">Fermer</b-button>
   </b-modal>
+
+  <b-modal id="bv-modal-updatetournament" hide-footer v-if="form.categories != []">
+<template slot="modal-title">
+Modifier une compétition
+</template>
+<div class="d-block text-center">
+<!----------------------DEBUT FORM MODAL-------------->
+<b-form @submit="onSubmit" @reset="onReset" v-if="show">
+  <b-form-group id="input-group-1" label="Nom de la compétition" label-for="input-1">
+    <b-form-input
+      id="input-1"
+      v-model="form.name"
+      required
+      placeholder="Entrer nom"
+    ></b-form-input>
+  </b-form-group>
+  <b-form-group id="input-group-2" label="Localisation:" label-for="input-2">
+    <b-form-input
+      id="input-2"
+      v-model="form.address"
+      required
+      placeholder="Enter name"
+    ></b-form-input>
+  </b-form-group>
+
+<b-form-group id="input-group-3" label="Catégories:" label-for="checkbox-group-1">
+    <b-form-checkbox-group
+            id="checkbox-group-1"
+            v-model="form.selectedCategories"
+            :options="form.categories"
+            name="flavour-1"
+    ></b-form-checkbox-group>
+</b-form-group>
+
+<b-form-group id="input-group-3" label="Date:" label-for="input-3">
+    <b-form-input
+            id="input-3"
+            v-model="form.date"
+            type="date"
+            required
+    ></b-form-input>
+</b-form-group>
+
+<b-form-group id="input-group-4" label="Heure:" label-for="input-4">
+    <b-form-input
+            id="input-4"
+            v-model="form.time"
+            type="time"
+            required
+    ></b-form-input>
+</b-form-group>
+
+<b-button type="submit" variant="primary" @click="$bvModal.hide('bv-modal-updatetournament')">Envoyer</b-button>
+</b-form>
+<!----------------------FIN FORM MODAL-------------->
+</div>
+<b-button class="mt-3" block @click="$bvModal.hide('bv-modal-updatetournament')">Fermer</b-button>
+</b-modal>
 
     </div>
 </template>
@@ -130,18 +190,33 @@
                       this.has_error = true
                   })
             },
+            deleteTournament(id){
+              var init = this;
+              this.$http({
+                  url: `tournament/delete/${id}`,
+                  method: 'GET'
+              })
+                  .then((res) => {
+                    console.log(res);
+                    init.getTournaments();
+                  }, () => {
+                      this.has_error = true
+                  })
+            },
             onSubmit(evt) {
             evt.preventDefault()
-            alert(JSON.stringify(this.form));
+            //alert(JSON.stringify(this.form));
             var init = this;
               evt.preventDefault()
               //alert(JSON.stringify(this.form));
-              this.$http.post('lesson/create',{
-                            idShooter: this.$auth.user.id,
-                            idCombatInstructor: this.form.instructor,
-                            comment: this.form.comment,
+              this.$http.post('tournament/create',{
+                            categories: this.form.selectedCategories.toString(),
+                            name: this.form.name,
+                            localisation: this.form.address,
+                            date: this.form.date,
+                            hour: this.form.time,
               }).then(function (response) {
-                init.getLessons();
+                init.getTournaments();
                   console.log(response.data);
                 },function (response) {
                   alert("ERREUR ESSAYE ENCORE")
